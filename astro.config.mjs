83c92cd77917fcb2895cfe895aaafce5ad155fd4
unknown <iadso.ao@gmail.com>
2023-08-20 23:27:44 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Adsoog.github.io',
-  base: '/Portfolio-Astro',
+  base: '/portfolio',
 });
